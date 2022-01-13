fea135aee3694f156369628a4935f453e7dd7b9e
jondgons <jondgons@gmail.com>
2022-01-13 16:54:53 -0600

    checker for validity of certain strings

---
+++ project0/src/main/java/com/revature/project0/Checker.java
@@ -0,0 +1,42 @@
+package com.revature.project0;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Checker contains a static helper method to
+ *   check for validity of certain strings.
+ */
+public class Checker {
+    /**
+     * Checks if a string is valid for a given type.
+     *   Valid email indicates letters or numbers + @ +
+     *     letters or numbers + . + letters or numbers.
+     *   Valid name indicates standard alphabetic characters.
+     * @param str string to check if of valid type
+     * @return true  - string is of valid type
+     *         false - string is not of valid type
+     */
+    public static boolean isValid(String str, String type) {
+        // start with empty string
+        String patternString = "";
+
+        // determine what patternString given a type
+        //   invalid type returns false
+        if (type.matches("email")) {
+            patternString = "[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]+";
+        } else if (type.matches("name")) {
+            patternString = "[a-zA-Z]+";
+        } else {
+            return false;
+        }
+
+        // create pattern & matcher given patternString
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(str);
+
+        // return result of matcher
+        return matcher.find();
+    }
+
+}
